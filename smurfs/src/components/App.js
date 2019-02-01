24a89baf08162c8dc5c13af25d32ebedc0a56563
joshua-disney <joshua.a.disney@gmail.com>
2019-02-01 10:11:57 -0700

    rendered Smurfs array to the screen.

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { getSmurfs } from '../actions';
 import './App.css';
 /*
  to wire this component up you're going to need a few things.
@@ -7,16 +10,40 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+
+  componentDidMount() {
+    console.log('didMount', this.props)
+    this.props.getSmurfs()
+  }
+
   render() {
+    console.log('inRender', this.props)
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <h1>SMURF VILLAGE</h1>
+        {this.props.smurfs.map(smurf => (
+          <div
+            className='smurfCard'
+            key={smurf.id}
+          >
+            <h4 className='smurfName'>{smurf.name}</h4>
+            <p className='smurfAge'>{smurf.age}</p>
+            <p className='smurfHeight'>{smurf.height}</p>
+          </div>
+        ))}
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return ({
+    isFetching: state.isFetching,
+    smurfs: state.smurfs
+  })
+}
+
+export default connect(
+  mapStateToProps,
+  { getSmurfs }
+)(App)
